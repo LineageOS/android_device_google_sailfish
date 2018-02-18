67927137954fdda7d919bcdfaf685e1efcda2d7f
Ethan Chen <intervigil@gmail.com>
2018-02-18 15:11:14 -0800

    sailfish: Update fingerprint
    Change-Id: Ib314bd56d2d8261aa4ba002dc103f09e9b6a2496

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:8.0.0/OPR3.170623.013/4397526:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 8.0.0 OPR3.170623.013 4397526 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:8.1.0/OPM1.171019.016/4503492:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM1.171019.016 4503492 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
