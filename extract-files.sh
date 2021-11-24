2e571c40d5c4e480e56fe90501e792c4f5d81db3
Michael Bestas <mkbestas@lineageos.org>
2021-11-24 23:31:19 +0200

    sailfish: Add extract-files.sh helper script
    Change-Id: Ie5f4c6ab8debf1a2b11db45b0cb6957e2539ac80
    (cherry picked from commit 40bffd6293c1d33acf397fd338b7f04c4d13b156)

--- extract-files.sh
+++ extract-files.sh
@@ -1,1 +1,13 @@
-../marlin/sailfish/extract-files.sh
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+"${MY_DIR}/../marlin/sailfish/extract-files.sh" "$@"
